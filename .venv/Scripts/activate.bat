6999f3aafb7415606a22f409c9475aa015ad3e42
Rodss2021 <rodstrab@gmail.com>
2024-04-30 12:36:03 -0300

    Acerto na navbar

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rodol\Documents\Python\AgendamentoPI\.venv
+set VIRTUAL_ENV=C:\Users\ANALISTA\Documents\Python\AgendamentoPI\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
